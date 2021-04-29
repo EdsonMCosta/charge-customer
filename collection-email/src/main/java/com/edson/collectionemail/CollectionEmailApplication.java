b84b497c8afe9ca260cb52de92eb6a9f68adcd08
Edson M Costa <edsoncosta.jr89@gmail.com>
2021-04-29 20:36:14 -0300

    First commit

---
+++ collection-email/src/main/java/com/edson/collectionemail/CollectionEmailApplication.java
@@ -0,0 +1,13 @@
+package com.edson.collectionemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionEmailApplication.class, args);
+	}
+
+}
---
+++ collection-letter/src/test/java/com/edson/collectionletter/CollectionLetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edson.collectionletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionLetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
